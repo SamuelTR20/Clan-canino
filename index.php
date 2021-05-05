ff8d5a36ffb08b32315b30a42375984d96b7497d
Samuel Torres <samy249@hotmail.com>
2021-05-05 09:22:49 -0700

    Search in index

--- index.php
+++ index.php
@@ -71,17 +71,21 @@ $mascotas = getMascotas($busqueda, $mostrar , $maximo);
       </div>
     </section>
 
-    <div class="s010  d-flex justify-content-center ">
-<form>
+    <div class="s010  d-flex justify-content-center bg-light">
+<form action="index.php" method="GET">
 <div class="inner-form  ">
 <div class="basic-search ">
 <div class="input-field">
-<input id="search" type="text" placeholder="Type Keywords">
+<input id="search" type="text" placeholder="Buscar" name='buscar'>
+<input type="text" hidden name = "numPag" value="1">
+
 <div class="icon-wrap">
+<button type="submit" value="numPag" class="ganbate" >
 <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
 <path d="M15.5 14h-.79l-.28-.27C15.41 12.59 16 11.11 16 9.5 16 5.91 13.09 3 9.5 3S3 5.91 3 9.5 5.91 16 9.5 16c1.61 0 3.09-.59 4.23-1.57l.27.28v.79l5 4.99L20.49 19l-4.99-5zm-6 0C7.01 14 5 11.99 5 9.5S7.01 5 9.5 5 14 7.01 14 9.5 11.99 14 9.5 14z"></path>
 </svg>
 </div>
+
 </div>
 </div>
 </form>
